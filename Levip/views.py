cc6ee96469976b125cfa374ac973614f889b019d
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-09-29 22:46:09 -0500

    Login implementado

--- Levip/views.py
+++ Levip/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+#
+# Rutas que no necesitan protección de rutas
+#
 def index(request):
     return render(request, 'home.html')
 
@@ -11,3 +15,24 @@ def about(request):
 def signin(request):
     return render(request, 'signin.html')
 
+def loginPage(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/levip/dashboard') 
+    else:
+        return redirect('/levip/signin')
+
+#
+# Rutas que necesitan protección de rutas
+#
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+@login_required
+def logoutPage(request):
+    logout(request)
+    return redirect('/levip/signin')
