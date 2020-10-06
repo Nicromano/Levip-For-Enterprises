e75e36feff4db1fa702412c8bc6911346a25b44d
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-10-06 00:26:37 -0500

    migracion

--- Levip/admin.py
+++ Levip/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Cliente, Administrador, Item, Despachador, Precio, Orden, Direccion, Pedido
+from .models import Cliente, Pedido
 
 admin.site.register(Cliente)
-admin.site.register(Administrador)
-admin.site.register(Item)
-admin.site.register(Despachador)
-admin.site.register(Precio)
-admin.site.register(Orden)
-admin.site.register(Direccion)
 admin.site.register(Pedido)
