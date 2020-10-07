25db4cc4cc0bc69cef867ecfb0c694a1b0ee3aa0
Nicromano <40124504+Nicromano@users.noreply.github.com>
2020-10-06 19:33:12 -0500

    lista migraciones

--- Levip/models.py
+++ Levip/models.py
@@ -2,17 +2,17 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    CLIE_CEDULA = models.CharField(max_length = 10, primary_key=True), 
-    CLIE_USERNAME = models.CharField(max_length= 50, default=None), 
-    CLIE_NOMBRE = models.CharField(max_length=50), 
-    CLIE_APELLIDO = models.CharField(max_length=50), 
-    CLIE_EMAIL = models.CharField(max_length=50), 
+    CLIE_CEDULA = models.CharField(max_length = 10, primary_key=True, default=None)
+    CLIE_USERNAME = models.CharField(max_length= 50, default=None)
+    CLIE_NOMBRE = models.CharField(max_length=50, default=None)
+    CLIE_APELLIDO = models.CharField(max_length=50, default=None) 
+    CLIE_EMAIL = models.CharField(max_length=50, default=None)
     CLIE_PASSWORD = models.TextField(default=None)
 
 class Item(models.Model):
     IT_ID = models.CharField(max_length = 5, primary_key=True)
-    IT_DESCRIPCION =  models.TextField(default=None), 
-    IT_PRECIO = models.DecimalField(max_digits=10, decimal_places=2)
+    IT_DESCRIPCION =  models.TextField(default=None)
+    IT_PRECIO = models.DecimalField(max_digits=10, decimal_places=2, default=None)
 
 class Pedido(models.Model):
     PED_ID = models.CharField(max_length = 5, primary_key=True)
